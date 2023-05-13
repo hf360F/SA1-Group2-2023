56702cf9055a8c104a51e6bc24a624796e8c533d
katja31ruda <ksr31@cam.ac.uk>
2023-05-13 17:03:13 +0100

    tidy A

--- build_lhs.m
+++ build_lhs.m
@@ -9,16 +9,13 @@ psip = zeros(np, np+1);
 
 for  i = 1:(np)
     for j = 1:(np+1)
-        if j == 1
+        if j < (np+1)
             [infa, infb] = panelinf(xs(j), ys(j), xs(j+1), ys(j+1), xs(i), ys(i));
-            psip(i, j) = infa;
+            psip(i, j) = psip(i, j) + infa;
+            psip(i, j+1) = infb;
          elseif j == np+1
              [infa, infb] = panelinf(xs(j-1), ys(j-1), xs(j), ys(j), xs(i), ys(i));
             psip(i, j) = infb;
-        else
-            [infa, infb2] = panelinf(xs(j), ys(j), xs(j+1), ys(j+1), xs(i), ys(i));
-            [infa2, infb] = panelinf(xs(j-1), ys(j-1), xs(j), ys(j), xs(i), ys(i));
-            psip(i, j) = infa + infb;
         end
     end
 end
