d143941ca02d097d7d189232400e2c83fa41b00c
Henry Free <hf360@cam.ac.uk>
2023-05-18 11:12:03 +0100

    Start W2ex2

--- exercise2.m
+++ exercise2.m
@@ -1,19 +1,23 @@
 % Test of ueintbit.m with constant ue
 
-ReL = 2500;
+ReL = 1E6;
 L = 1;
 
 nx = 101;
 
-ue = linspace(1, 1, nx);
-x = linspace(0, 1, nx);
+ue = linspace(1, 1-0.2, nx);
+x = linspace(0, 1/L, nx);
 theta = zeros(nx, 1);
 thetaBlas = zeros(nx, 1);
 ReTheta = zeros(nx, 1);
 
 intTot = 0;
+i = 1;
+laminar = true;
 
-for i = 1:(length(x)-1)
+while laminar && i < (nx-1)
+    i = i + 1;
+
     ua = ue(i);
     ub = ue(i+1);
     xa = x(i);
@@ -21,21 +25,25 @@ for i = 1:(length(x)-1)
 
     % Momentum thickness by Blasius, Thwaites
     intTot = intTot + ueintbit(xa, ua, xb, ub); % Euler for Thwaites integral
-    theta(i) = L*(0.45*(ua^(-6))*intTot/ReL)^(0.5);
-    thetaBlas(i) = L*0.664*(xa^0.5)/(ReL^(0.5));
+    theta(i) = (0.45*(ua^(-6))*intTot/ReL)^(0.5);
+    thetaBlas(i) = 0.664*(xa^0.5)/(ReL^(0.5));
 
     ReTheta(i) = ReL*ua*theta(i);
 
-    m = 
+    % Find energy shape factor from Thwaites gradient factor
+    m = -ReL*(theta(i)/L)^2*(ub-ua)/(xb-xa);
+    H = thwaites_lookup(m);
+    He = laminar_He(H);
     
-
+    if log(ReTheta(i)) >= 18.4*He - 21.74
+        laminar = false;
+        disp([xa ReTheta(i)/1000])
+    end
 end
 
-%theta
-
-plot(x/L, theta/L)
+plot(x, theta)
 hold on
-plot(x/L, thetaBlas/L)
+plot(x, thetaBlas)
 xlabel("x/L")
 ylabel("theta/L")
 legend("Thwaites", "Blasius")
